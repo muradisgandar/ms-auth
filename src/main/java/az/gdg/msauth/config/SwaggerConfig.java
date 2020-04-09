7cec1055ac7567efc80912f8943c4e81afd53a6e
muradisgandar <isgandarli_murad@mail.ru>
2020-04-09 18:10:00 +0400

    dev and prod profile created

--- src/main/java/az/gdg/msauth/config/SwaggerConfig.java
+++ src/main/java/az/gdg/msauth/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package az.gdg.msauth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -13,6 +14,7 @@ import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
+@Profile("!prod")
 public class SwaggerConfig {
 
     @Bean
