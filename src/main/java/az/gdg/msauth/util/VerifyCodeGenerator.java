761686fc3da2e2b9332f82669412ce3685a2d8e5
muradisgandar <isgandarli_murad@mail.ru>
2020-04-14 01:32:42 +0400

    changes

--- src/main/java/az/gdg/msauth/util/VerifyCodeGenerator.java
+++ src/main/java/az/gdg/msauth/util/VerifyCodeGenerator.java
@@ -7,7 +7,7 @@ public class VerifyCodeGenerator {
     public static String generatedCode() {
         int leftLimit = 48;
         int rightLimit = 122;
-        int targetStringLength = 120;
+        int targetStringLength = 46;
         Random random = new Random();
         StringBuilder buffer = new StringBuilder(targetStringLength);
         for (int i = 0; i < targetStringLength; i++) {
